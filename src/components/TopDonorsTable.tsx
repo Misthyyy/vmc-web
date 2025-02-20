d4bf99aa3164734fd05420be7353e1e89b556160
Misthyyy <misthycddg@gmail.com>
2025-02-20 09:51:27 +0700

    Update TopDonorsTable.tsx

--- src/components/TopDonorsTable.tsx
+++ src/components/TopDonorsTable.tsx
@@ -62,7 +62,7 @@ const TopDonorsTable = () => {
           mb: 2,
         }}
       >
-        Last updated 19/02/2025 10:00 AM
+        Last updated 20/02/2025 9:30 AM
       </Typography>
       <TableContainer sx={{ p: 1 }}>
         <Table>
