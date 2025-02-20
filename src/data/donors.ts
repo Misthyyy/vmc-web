b3cbf298ff577562884abd7d832c124fc6228d2c
Misthyyy <misthycddg@gmail.com>
2025-02-20 09:50:42 +0700

    Update donors.ts

--- src/data/donors.ts
+++ src/data/donors.ts
@@ -40,32 +40,40 @@ export const milestones: Milestone[] = [
 
 export const donors: Donor[] = [
   { id: 1, name: "ArtR", amount: 10000085 },
-  { id: 2, name: "Linh Vũ", amount: 3000000 },
-  { id: 3, name: "Key", amount: 1183860 },
-  { id: 4, name: "Nhoxlove", amount: 1000000 },
-  { id: 5, name: "HPhuong", amount: 777777 },
-  { id: 6, name: "kurumi_1504", amount: 536462 },
-  { id: 7, name: "Pông", amount: 500000 },
-  { id: 8, name: "Gòl", amount: 400000 },
-  { id: 9, name: "Glaukopis", amount: 321195 },
-  { id: 10, name: "Natraaa", amount: 300000 },
-  { id: 11, name: "ngaan", amount: 202500 },
-  { id: 12, name: "Dahn", amount: 200000 },
-  { id: 13, name: "Tidal", amount: 200000 },
-  { id: 14, name: "Bò Núc Nắc", amount: 173262 },
-  { id: 15, name: "YoungHa", amount: 150000 },
-  { id: 16, name: "Naa", amount: 150000 },
-  { id: 17, name: "novbong", amount: 150000 },
-  { id: 18, name: "Tôm", amount: 150000 },
-  { id: 19, name: "Dứa", amount: 100000 },
-  { id: 20, name: "Hải My", amount: 100000 },
-  { id: 21, name: "Hiền Kun", amount: 100000 },
-  { id: 22, name: "Hải My", amount: 100000 },
-  { id: 23, name: "itshienkun", amount: 100000 },
-  { id: 24, name: "Tus", amount: 100000 },
-  { id: 25, name: "tisBer", amount: 50000 },
-  { id: 26, name: "Thư Huỳnh", amount: 50000 },
-  { id: 27, name: "Chichoo", amount: 20000 },
+  { id: 2, name: "ChanNee", amount: 5007574 },
+  { id: 3, name: "Linh Vũ", amount: 3000000 },
+  { id: 4, name: "Key", amount: 1183860 },
+  { id: 5, name: "Nhoxlove", amount: 1000000 },
+  { id: 6, name: "HPhuong", amount: 777777 },
+  { id: 7, name: "kurumi_1504", amount: 536462 },
+  { id: 8, name: "Pông", amount: 500000 },
+  { id: 9, name: "Sherry", amount: 500000 },
+  { id: 10, name: "Gòl", amount: 400000 },
+  { id: 11, name: "Glaukopis", amount: 321195 },
+  { id: 12, name: "Natraaa", amount: 300000 },
+  { id: 13, name: "Thi", amount: 290125 },
+  { id: 14, name: "tisBer", amount: 250000 },
+  { id: 15, name: "ngaan", amount: 202500 },
+  { id: 16, name: "Dahn", amount: 200000 },
+  { id: 17, name: "Tidal", amount: 200000 },
+  { id: 18, name: "Bò Núc Nắc", amount: 173262 },
+  { id: 19, name: "YoungHa", amount: 150000 },
+  { id: 20, name: "Naa", amount: 150000 },
+  { id: 21, name: "novbong", amount: 150000 },
+  { id: 22, name: "Tôm", amount: 150000 },
+  { id: 23, name: "Dứa", amount: 100000 },
+  { id: 24, name: "Hải My", amount: 100000 },
+  { id: 25, name: "Hiền Kun", amount: 100000 },
+  { id: 26, name: "Hải My", amount: 100000 },
+  { id: 27, name: "itshienkun", amount: 100000 },
+  { id: 28, name: "Tus", amount: 100000 },
+  { id: 29, name: "nờ tờ", amount: 100000 },
+  { id: 30, name: "Gạo", amount: 55555 },
+  { id: 31, name: "Sườn Chìa", amount: 50000 },
+  { id: 32, name: "Thư Huỳnh", amount: 50000 },
+  { id: 33, name: "Gắn chúa 🐍", amount: 50000 },
+  { id: 34, name: "Chichoo", amount: 20000 },
+  { id: 35, name: "Ngaan", amount: 20000 }
 ];
 
 // Get total goal from the last milestone (highest amount)
@@ -77,6 +85,6 @@ export const totalGoal = Math.max(...milestones.map((m) => m.amount));
 //   0
 // );
 
-export const donationAmount = 36549499;
+export const donationAmount = 42482628;
 // Calculate percentage of goal reached
 export const percentage = (donationAmount / totalGoal) * 100;
